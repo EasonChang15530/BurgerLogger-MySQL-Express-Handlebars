8e77f2e77daa0bc184753b0c7fa9b4800dcf9a31
eason <eason15530@gmail.com>
2019-11-26 22:06:24 -0500

    Set up the database and connection

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+// setup the code to connect Node to MySQL.
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: process.env.mysql_pass,
+  database: "burgers_db"
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
