7c211eced13c54ff2a9e71e0ca4f42c53c362e99
eason <eason15530@gmail.com>
2019-12-01 11:30:26 -0500

    debug

--- config/connection.js
+++ config/connection.js
@@ -6,6 +6,7 @@ var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB);
+  console.log(process.env.JAWSDB_URL)
 } else {
   connection = mysql.createConnection({
     host: "localhost",
